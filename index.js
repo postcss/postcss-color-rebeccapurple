5ca4e92e7a4df5bc320a412f6bdb90ad742b9017
Jonathan Neal <jonathantneal@hotmail.com>
2018-05-01 07:46:56 -0400

    Protect decl.value from parsing with regex like postcss-custom-properties
    https://github.com/postcss/postcss-custom-properties/blob/master/index.js#L6

--- index.js
+++ index.js
@@ -4,6 +4,7 @@
 const postcss = require("postcss")
 const valueParser = require("postcss-value-parser")
 const color = "#639"
+const regexp = /(^|[^\w-])rebeccapurple([^\w-]|$)/
 
 /**
  * PostCSS plugin to convert colors
@@ -12,7 +13,7 @@ module.exports = postcss.plugin("postcss-color-rebeccapurple", () => (style) => 
   style.walkDecls((decl) => {
     const value = decl.value;
 
-    if (value && value.indexOf("rebeccapurple") !== -1) {
+    if (value && regexp.test(value)) {
       decl.value = valueParser(value).walk((node) => {
         if (node.type === "word" && node.value === "rebeccapurple") {
           node.value = color
